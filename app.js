e4ca61c446eaf490d8b24255b68adbbba2ff24ac
Facundo Erbin <facuerbin@gmail.com>
2020-06-20 03:02:22 -0300

    Express Hello World Web Server!

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+#NodeJS Express Framework Webapp
+#
+@author: Facundo Erbin
+*/
+
+const express = require ('express');
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("./index.html"));
+
+app.listen(port, ()=> console.log(`Example app listening at port ${port}`));
